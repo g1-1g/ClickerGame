24b9c8019f5f240d88a1c15df00a2f685a8f9b3e
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-02-10 13:05:07 +0900

    Chore : Unitask 방식으로 변경

--- Assets/WebAPITurtorial/WebGetTextTest.cs
+++ Assets/WebAPITurtorial/WebGetTextTest.cs
@@ -1,14 +1,23 @@
 using System.Collections;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class WebGetTextTest : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private async void Start()
     {
         //서버에게 데이터 요청하는 작업은 비동기 => 코루틴으로 처리
-        StartCoroutine(GetText());
+        string text = await GetWebText("https://www.google.com/search?q=%EB%BD%80%EB%A1%9C%EB%A1%9C&sca_esv=e5f3872605fd4577&hl=ko&biw=1745&bih=859&sxsrf=ANbL-n4R8XM_m_xpv9kQtP3kiSLcrl5pBw%3A1770696215975&ei=F66KaZ-ZO-Hi2roPjbClkAg&ved=0ahUKEwjfgNW3hc6SAxVhsVYBHQ1YCYIQ4dUDCBE&uact=5&oq=%EB%BD%80%EB%A1%9C%EB%A1%9C&gs_lp=Egxnd3Mtd2l6LXNlcnAiCeu9gOuhnOuhnDIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYR0jRCFDQBFj7BnACeAGQAQCYAWigAdABqgEDMC4yuAEDyAEA-AEBmAIEoALaAcICChAuGIAEGEMYigXCAgoQABiABBhDGIoFwgIFEAAYgATCAggQLhiABBixA8ICGRAuGIAEGEMYigUYlwUY3AQY3gQY4ATYAQGYAwDiAwUSATEgQIgGAZAGCroGBggBEAEYFJIHAzIuMqAH6RKyBwMwLjK4B9YBwgcDMC40yAcHgAgA&sclient=gws-wiz-serp");
+        Debug.Log(text);
+    }
+
+    public async UniTask<string> GetWebText(string url)
+    {
+        var result = (await UnityWebRequest.Get(url).SendWebRequest()).downloadHandler.text;
+        return result;
     }
 
     IEnumerator GetText()

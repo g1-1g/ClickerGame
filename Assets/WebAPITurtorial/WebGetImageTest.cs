f79e1ce3f0ebdea83c68c005763073a250f4c3f7
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-02-10 13:17:50 +0900

    Feat : Image Get

---
+++ Assets/WebAPITurtorial/WebGetImageTest.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class WebGetImageTest : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage _myImage;
+
+    async void Start()
+    {
+        Texture myTexture = await GetWebTexture("https://image.aladin.co.kr/product/25063/38/cover500/8964135725_1.jpg");
+        _myImage.texture = myTexture;
+    }
+
+    async UniTask<Texture> GetWebTexture(string url)
+    {
+        var result = (await UnityWebRequestTexture.GetTexture(url).SendWebRequest());
+        if (result.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(result.error);
+            return null;
+        }
+        else
+        {
+            return ((DownloadHandlerTexture)result.downloadHandler).texture;
+        }
+    }
+    IEnumerator GetTexture(string url)
+    {
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            _myImage.texture = myTexture;
+        }
+    }
+}
